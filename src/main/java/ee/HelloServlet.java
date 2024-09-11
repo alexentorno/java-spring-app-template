ff8ced0425734e15e4ebb68b192fcea521cc17ea
alpekh <alpekh@taltech.ee>
2024-09-11 16:29:00 +0300

    add tag

--- src/main/java/ee/HelloServlet.java
+++ src/main/java/ee/HelloServlet.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 @WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
 
+
     @Override
     public void doGet(HttpServletRequest request,
                       HttpServletResponse response)
