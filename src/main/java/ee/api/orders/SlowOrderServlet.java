be7f28a3f87cfa3d300d2270cb0514f9e632364d
alpekh <alpekh@taltech.ee>
2024-10-03 13:36:27 +0300

    made connection pool singleton

--- src/main/java/ee/api/orders/SlowOrderServlet.java
+++ src/main/java/ee/api/orders/SlowOrderServlet.java
@@ -4,7 +4,6 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.apache.commons.dbcp2.BasicDataSource;
 import util.ConnectionPoolFactory;
 
 import javax.sql.DataSource;
@@ -14,15 +13,15 @@ import java.sql.SQLException;
 
 @WebServlet("/api/orders/slow")
 public class SlowOrderServlet extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         DataSource pool = new ConnectionPoolFactory().createConnectionPool();
 
         try (Connection conn = pool.getConnection()) {
+//            System.out.println("Got a connection from the pool");
 //            printPoolInfo(pool);
-
             Thread.sleep(1000);
-
         } catch (SQLException | InterruptedException e) {
             resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
             resp.getWriter().write("{ \"error\": \"Internal Server Error\" }");
--- src/main/java/util/ConnectionPoolFactory.java
+++ src/main/java/util/ConnectionPoolFactory.java
@@ -6,19 +6,27 @@ import javax.sql.DataSource;
 import java.sql.SQLException;
 
 public class ConnectionPoolFactory {
+    private static volatile BasicDataSource pool; //Singleton instance of the connection pool
+
     public DataSource createConnectionPool() {
-        ConnectionInfo connectionInfo = ConfigUtil.readConnectionInfo();
-        BasicDataSource pool = new BasicDataSource();
-        pool.setDriverClassName("org.postgresql.Driver");
-        pool.setUrl(connectionInfo.getUrl());
-        pool.setUsername(connectionInfo.getUser());
-        pool.setPassword(connectionInfo.getPass());
-        pool.setMaxTotal(2);
-        pool.setInitialSize(2);
-        try {
-            pool.getLogWriter();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+        if (pool == null) {
+            synchronized (ConnectionPoolFactory.class) {
+                if (pool == null) {
+                    pool = new BasicDataSource();
+                    ConnectionInfo connectionInfo = ConfigUtil.readConnectionInfo();
+                    pool.setDriverClassName("org.postgresql.Driver");
+                    pool.setUrl(connectionInfo.getUrl());
+                    pool.setUsername(connectionInfo.getUser());
+                    pool.setPassword(connectionInfo.getPass());
+                    pool.setMaxTotal(2);
+                    pool.setInitialSize(2);
+                    try {
+                        pool.getLogWriter();
+                    } catch (SQLException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
         }
         return pool;
     }
