a74e46a14075bc38601e00b0dd15ff9878b84521
alpekh <alpekh@taltech.ee>
2024-10-20 19:18:41 +0300

    Hw07. Removed connectionInfo from SlowOrderServlet

--- src/main/java/ee/api/orders/SlowOrderServlet.java
+++ src/main/java/ee/api/orders/SlowOrderServlet.java
@@ -4,38 +4,20 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import util.ConnectionPoolFactory;
 
-import javax.sql.DataSource;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.SQLException;
+
 
 @WebServlet("/api/orders/slow")
 public class SlowOrderServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        DataSource pool = new ConnectionPoolFactory().createConnectionPool();
-
-        try (Connection conn = pool.getConnection()) {
-//            System.out.println("Got a connection from the pool");
-//            printPoolInfo(pool);
+        try {
             Thread.sleep(1000);
-        } catch (SQLException | InterruptedException e) {
+        } catch ( InterruptedException e) {
             resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
             resp.getWriter().write("{ \"error\": \"Internal Server Error\" }");
         }
     }
-
-//    private static void printPoolInfo(DataSource dataSource) {
-//        if (!(dataSource instanceof BasicDataSource)) {
-//            throw new IllegalArgumentException("argument must be BasicDataSource");
-//        }
-//
-//        BasicDataSource pool = (BasicDataSource) dataSource;
-//
-//        System.out.printf("active: %s; idle: %s\n",
-//                pool.getNumActive(), pool.getNumIdle());
-//    }
 }
